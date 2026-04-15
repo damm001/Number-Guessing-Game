a8aa6c8b292e04ec0b27eb2b8ebcc30fd2134e59
Steph Otunla <tunesbaby7175@gmail.com>
2026-04-15 02:54:13 -0400

    feat: create function to save the game

--- number_guess.sh
+++ number_guess.sh
@@ -118,3 +118,15 @@ SAVE_USER() {
   fi
   SAVE_GAME $USER_NAME $GUESS_COUNT
 }
+
+SAVE_GAME() {
+  USER_NAME=$1 
+  NUMBER_OF_GUESSES=$2
+
+  USER_ID=$($PSQL "SELECT user_id FROM users WHERE username='$USER_NAME';")
+  INSERT_GAME=$($PSQL "INSERT INTO games(user_id, best_guess) VALUES($USER_ID, $NUMBER_OF_GUESSES);")
+  USER_NAME=$($PSQL "SELECT username FROM users WHERE user_id=$USER_ID;")
+}
+
+
+INPUT_NAME
